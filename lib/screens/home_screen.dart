203c103707161163a7c721bf06bc05e40e1e65ba
waldson fagundes <waldsonfagundes@waldsons-Mac-mini.local>
2023-11-27 23:10:55 -0300

    adjust the HomeScreen UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,43 +1,118 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_blurhash/flutter_blurhash.dart';
 
 class HomeScreen extends StatelessWidget {
-     HomeScreen({super.key});
+  HomeScreen({super.key});
 
-    final List<String> coversTest = [
-      'https://www.gutenberg.org/cache/epub/72134/pg72134.cover.medium.jpg',
-      'https://www.gutenberg.org/cache/epub/72127/pg72127.cover.medium.jpg',
+  final List<String> covers = [
+    'https://www.gutenberg.org/cache/epub/72134/pg72134.cover.medium.jpg',
+    'https://www.gutenberg.org/cache/epub/72127/pg72127.cover.medium.jpg',
+    "https://www.gutenberg.org/cache/epub/72126/pg72126.cover.medium.jpg",
+    "https://www.gutenberg.org/cache/epub/63606/pg63606.cover.medium.jpg",
+    "https://www.gutenberg.org/cache/epub/72135/pg72135.cover.medium.jpg",
+    "https://www.gutenberg.org/cache/epub/18452/pg18452.cover.medium.jpg",
+  ];
 
 
-    ];
-
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title:  const Text('Minha Biblioteca'),
-        ),
-        body: Padding(
-          padding:const EdgeInsets.all(16.0),
-          child: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              crossAxisSpacing: 10.0,
-              mainAxisSpacing: 10.0,
+ @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Minha Biblioteca'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                ElevatedButton(onPressed: (){}, child: Text('Livros')),
+                SizedBox(width: 6,),
+                ElevatedButton(onPressed: (){}, child: Text('Favoritos')),
+              ],
             ),
-            itemCount: coversTest.length,
-            itemBuilder: (BuildContext context, int index) {
-              return GestureDetector(
-                onTap: () {
-                 //TODO: implementar acesso ao conteudo do livro
-                },
-                child: Image.network(
-                  coversTest[index],
-                  fit: BoxFit.cover,
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 12.0,
+                  mainAxisSpacing: 12.0,
                 ),
-              );
-            },
-          ),
+                itemCount: covers.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return GestureDetector(
+                    onTap: () {
+                      //TODO: implementar acesso ao conteudo do livro
+                    },
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10.0),
+                      child: Container(
+
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: Colors.grey
+                        ),
+                        child: Stack (
+                          fit: StackFit.expand,
+                          children: [
+
+
+                            Image.network(
+                              covers[index],
+                              fit: BoxFit.contain,
+                            ),
+                            const Positioned(
+                              top: 0,
+                              right: 8.0,
+                              child: InkWell(
+                               //TODO: Implementar
+                                child: Icon(
+                                  Icons.bookmark,
+                                  color: Colors.amber,
+                                  size: 32.0,
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              bottom: 0,
+                              left: 0,
+                              right: 0,
+                              child: Container(
+                                color: Colors.black.withOpacity(0.6),
+                                child:  const Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children:  [
+                                    Text(
+                                      'Title',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      'Author',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
-      );
-    }
+      ),
+    );
+  }
 }
+
